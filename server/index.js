98ea12fa8f2f47988364e6a0c0c58d9cf21e4479
Greg Sithole <gregsithole@hotmail.co.za>
2022-07-24 23:11:40 +0200

    Added Example Post Routes

--- server/index.js
+++ server/index.js
@@ -4,15 +4,19 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import postRoutes from './routes/posts.js';
+
 dotenv.config();
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 const CONNECTION_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ep9aziz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true`;
-const PORT = process.env.PORT || 3020;
+const PORT = process.env.PORT || 4000;
 
 mongoose
     .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
