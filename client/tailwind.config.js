5fa9cba7ccef9fc0f1d2e6c5eb8e059191015a70
Greg Sithole <gregsithole@hotmail.co.za>
2022-07-25 00:10:57 +0200

    Added Typography to Tailwind Config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [require('@tailwindcss/typography'), require("daisyui")],
 }
