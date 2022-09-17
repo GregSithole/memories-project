b339f253cc9fe5af789f2886b1de1ae6cb51c6e2
Greg Sithole <gregsithole@hotmail.co.za>
2022-09-17 20:41:36 +0200

    Updated index and app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Typography } from '@material-tailwind/react';
+import { Container, AppBar, Grow, Grid} from '@material-ui/core'
+
+import memories from './images/memories.png'
 
 const App = () => {
     return (
-        <div>App</div>
+        <Container maxWidth='lg'>
+			<AppBar position='static' color='inherit'>
+				<Typography variant='h2' align='center'>Memories</Typography>
+				<img src={memories} alt='memores' height='60' />
+			</AppBar>
+			<Grow in>
+				<Container>
+					<Grid container justifyContent='space-between' alignItems='strech' spacing={3}>
+						<Grid item xs={12} sm={7}>
+							<Post />
+						</Grid>
+						<Grid item xs={12} sm={4}>
+							<Form />
+						</Grid>
+					</Grid>
+				</Container>
+			</Grow>
+		</Container>
     )
 }
 
-export default App
+export default App;
